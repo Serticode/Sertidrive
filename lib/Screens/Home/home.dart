7c890345066bfc70af010c1e8921bb10161fa7b9
Akujor Samuel O <helpdeskforserticode@gmail.com>
2021-03-20 22:01:26 +0100

    Added a custom clipper to the home screen

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -167,15 +167,49 @@ class _HomeState extends State<Home> {
             },
           )
         ]),
-        body: Container(
-          padding: EdgeInsets.all(15.0),
-          child: baseList(context: context, folders: rootFolders),
+        body: Column(
+          children: <Widget>[
+            curve(),
+            Expanded(
+              child: baseList(buildContext: context, folders: rootFolders),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Widget baseList({BuildContext context, List folders}) {
+  Widget curve() {
+    return Container(
+        child: Stack(
+      children: <Widget>[
+        //stack overlaps widgets
+        Opacity(
+          //semi red clipPath with more height and with 0.5 opacity
+          opacity: 0.5,
+          child: ClipPath(
+            clipper: WaveClipper(), //set our custom wave clipper
+            child: Container(
+              color: Theme.of(context).primaryColor,
+              height: 100,
+            ),
+          ),
+        ),
+
+        ClipPath(
+          //upper clipPath with less height
+          clipper: WaveClipper(), //set our custom wave clipper.
+          child: Container(
+            color: Colors.blue[800],
+            height: 90,
+            alignment: Alignment.center,
+          ),
+        ),
+      ],
+    ));
+  }
+
+  Widget baseList({BuildContext buildContext, List folders}) {
     return ListView.separated(
       separatorBuilder: (context, index) => Divider(
         color: Theme.of(context).primaryColor,
@@ -238,4 +272,40 @@ class _HomeState extends State<Home> {
 
   Widget getAudiosList() => Audios();
   Widget getDocumentsList() => Documents();
+}
+
+//Custom CLipper class with Path
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+    path.lineTo(
+        0, size.height); //start path with this if you are making at bottom
+
+    var firstStart = Offset(size.width / 5, size.height);
+    //fist point of quadratic bezier curve
+    var firstEnd = Offset(size.width / 2.25, size.height - 50.0);
+    //second point of quadratic bezier curve
+    path.quadraticBezierTo(
+        firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
+
+    var secondStart =
+        Offset(size.width - (size.width / 3.24), size.height - 105);
+    //third point of quadratic bezier curve
+    var secondEnd = Offset(size.width, size.height - 10);
+    //fourth point of quadratic bezier curve
+    path.quadraticBezierTo(
+        secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy);
+
+    path.lineTo(
+        size.width, 0); //end with this path if you are making wave at bottom
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false; //if new instance have different instance than old instance
+    //then you must return true;
+  }
 }
